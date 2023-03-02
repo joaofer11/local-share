07dd0de7992b2469310823a63730f807f21b975f
João Fernandes <joaofergear003@gmail.com>
2023-03-02 19:10:43 -0300

    fix: add all files implementations to commit

--- src/routes.ts
+++ src/routes.ts
@@ -1,19 +1,43 @@
 import { constructPath } from './utils/constructPath'
 import { IRoute } from './interfaces/IRoute'
 
+const mockData = [
+  {
+    id: 1,
+    name: 'Sam Fisher',
+    age: 50,
+    series: 'Splinter Cell' 
+  },
+  {
+    id: 2,
+    name: 'James Bond',
+    age: 'unknown',
+    series: '007' 
+  },
+]
+
 export const routes: IRoute[] = [
   {
     method: 'GET',
     path: constructPath('/characters'),
-    handler: (req, res) => {
-
+    handler: (_, res) => {
+      res.send!(200, mockData)
     }
   },
   {
     method: 'GET',
     path: constructPath('/characters/:characterId'),
     handler: (req, res) => {
-      res.end()
+      const { characterId } = req.pathParams!
+
+      const existingData = mockData.find(character => character.id === Number(characterId))
+      
+      if (!existingData) {
+        res.send!(404, { error: 'Character not found' })
+        return 
+      }
+
+      res.send!(200, existingData)
     }
   },
 ]
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,37 @@
-import http from 'http'
+import { findRoute } from './utils/findRoute'
+import { IRequest, IResponse, createServer } from 'node:http'
+import { bodyParserHelper } from './middlewares/BodyParserHelper'
+import { injectParamsHelper } from './middlewares/InjectParamsHelper'
+import { responseSendingHelper } from './middlewares/ResponseSendingHelper'
 
 const PORT = 3333
 
-const handleReq = (req: http.IncomingMessage, res: http.ServerResponse) => {
-  res.end(Buffer.from(`${req.method} at ${req.url}`))
+const handleReq = (req: IRequest, res: IResponse) => {
+  const urlInfo = new URL(`http://localhost:${PORT}${req.url}`)
+
+  responseSendingHelper.execute(res)
+
+  const existingRoute = findRoute(urlInfo.pathname, req.method!)
+  
+  if (!existingRoute) {
+    res.send!(404, { error: 'Route not found.' }) 
+    return
+  }
+  
+  injectParamsHelper.execute({
+    req,
+    urlInfo,
+    existingRoute 
+  })
+  
+  if (['POST', 'PUT', 'PATCH'].includes(req.method!)) {
+    bodyParserHelper.execute(req, () => existingRoute.handler(req, res))
+    return
+  }
+  
+  existingRoute.handler(req, res)
 }
 
-const server = http.createServer(handleReq)
+const server = createServer(handleReq)
 
 server.listen(PORT, () => console.log(`Server has started at http://localhost:${PORT}`))
